4303e662a0cef868197e6f2b20cea8496725fdf5
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2025-08-05 16:30:10 -0500

    feat: add middle position class for inheritance

--- virtualjoystick/src/main/java/com/yoimerdr/android/virtualjoystick/geometry/position/FixedPosition.kt
+++ virtualjoystick/src/main/java/com/yoimerdr/android/virtualjoystick/geometry/position/FixedPosition.kt
@@ -1,9 +1,9 @@
 package com.yoimerdr.android.virtualjoystick.geometry.position
 
-open class FixedPosition(
+class FixedPosition(
     override val x: Float,
-    override val y: Float
-) : ImmutablePosition {
+    override val y: Float,
+) : PartialPosition() {
 
     constructor() : this(0.0f, 0.0f)
 
@@ -11,25 +11,6 @@ open class FixedPosition(
 
     constructor(position: ImmutablePosition) : this(position.x, position.y)
 
-    override fun deltaX(x: Float): Float = this.x - x
-
-    override fun deltaX(position: ImmutablePosition): Float = deltaX(position.x)
-
-    override fun deltaY(y: Float): Float = this.y - y
-
-    override fun deltaY(position: ImmutablePosition): Float = deltaY(position.y)
-
-    override fun equals(other: Any?): Boolean {
-        if (other is ImmutablePosition)
-            return other.x == x && other.y == y
-        return super.equals(other)
-    }
-
     override fun toString(): String = "FixedPosition(x=%.2f,y=%.2f)".format(x, y)
 
-    override fun hashCode(): Int {
-        var result = x.hashCode()
-        result = 31 * result + y.hashCode()
-        return result
-    }
 }
